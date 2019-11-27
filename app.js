324a163f64d22a2496b2222cdf83dfc9f5e9806c
cweng777 <cweng777@gmail.com>
2019-11-28 00:06:00 +0800

    All files for homepage interface

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const port = 4000
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+//For css, javascript
+app.use('/static', express.static('public'));
+
 //Define Routes
 const login = require('./routes/login');
 const home = require('./routes/home');
+const homepage = require('./routes/homepage');
 app.use('/login', login);
 app.use('/', home);
+app.use('/homepage', homepage);
 
 app.listen(port, () => console.log(`App listening on port ${port}.`))
