fa29793bf3d7271d4641b2bee316b4839f5d52e5
Amy Suzuki <amy.s.suzuki@gmail.com>
2019-11-29 22:09:43 -0800

    Adjustments made to run in Heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs  = require('express-handlebars');
 
 const app = express();
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //Get Root of project
 var path = require('path');
