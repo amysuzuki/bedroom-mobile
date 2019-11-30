7d956e1168b15b4df5d8ee53bf9f77a032cada68
Amy Suzuki <amy.s.suzuki@gmail.com>
2019-11-29 21:44:34 -0800

    Add new routes for each app route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const exphbs  = require('express-handlebars');
 const app = express();
 const port = 4000
 
+//Get Root of project
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+
 //include body parser so we can parse the bodies of post requests...
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
@@ -19,12 +23,16 @@ app.use('/static', express.static('public'));
 
 //Define Routes
 const login = require('./routes/login');
-const home = require('./routes/home');
 const homepage = require('./routes/homepage');
-const filter = require('./routes/filterInterface')
+const filter = require('./routes/filterInterface');
+const utilities = require('./routes/utilities');
+const planner = require('./routes/planner');
+
 app.use('/login', login);
-app.use('/', home);
+app.use('/', login);
 app.use('/homepage', homepage);
+app.use('/planner', planner);
+app.use('/utilities', utilities);
 app.use('/filterInterface', filter);
 
 app.listen(port, () => console.log(`App listening on port ${port}.`))
