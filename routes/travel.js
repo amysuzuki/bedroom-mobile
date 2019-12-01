2bded3f0e6c3f13cec4fe12089d79204b461c6e8
Amy Suzuki <amy.s.suzuki@gmail.com>
2019-12-01 13:56:31 -0800

    Test to see if issue with Heroku is from static routes

--- routes/travel.js
+++ routes/travel.js
@@ -3,14 +3,13 @@ var router = express.Router();
 
 // define the home page route
 router.get('/', function (req, res) {
-  // let context = {};
-  // context.title="View the Current Bedroom-Mobile Route and Position";
-  // //context.greeting ="Enter your criteria";
-  // context.route = {time:"8pm Friday Morning",stop:"Grandma's House"};
-  // //context.registeredDocks = [{name: "exampleName", cost: 9.99}, {name: "exampleName2", cost: 9.99}, {name: "exampleName3", cost: 9.99}];
-  // 
-  // res.render('TravelPage', context);
-  res.render('login')
+  let context = {};
+  context.title="View the Current Bedroom-Mobile Route and Position";
+  //context.greeting ="Enter your criteria";
+  context.route = {time:"8pm Friday Morning",stop:"Grandma's House"};
+  //context.registeredDocks = [{name: "exampleName", cost: 9.99}, {name: "exampleName2", cost: 9.99}, {name: "exampleName3", cost: 9.99}];
+
+  res.render('TravelPage', context);
 });
 
 module.exports = router;
