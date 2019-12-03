e2e9a152360695537ee29e9213316f8c87c48115
Phil <knamustarsunder@gmail.com>
2019-12-02 18:44:20 -0800

    added a function to planner.js
    estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin and destination locations passed to it by using the AutoDriving system's mapping trip management software

--- routes/planner.js
+++ routes/planner.js
@@ -8,3 +8,12 @@ router.get('/', function (req, res) {
 });
 
 module.exports = router
+
+//estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin 
+//and destination locations passed to it by using the AutoDriving system's mapping trip management software
+function estimateTripFuelUse(origin, destination){
+  distance = bedroomMobile.autoDriver.getDistance(origin, destination);
+  fuelRequired = distance * bedroomMobile.utilities.avgMPG;
+  numRefuels = fuelRequired/ bedroomMobile.utilities.fuelCapacity;
+  return numRefuels;
+}
