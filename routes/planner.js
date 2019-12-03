f4c212df32dc2248fd448f5f14e7729c3eead547
Amy Suzuki <amy.s.suzuki@gmail.com>
2019-12-02 21:34:11 -0800

    Travel Planner Page implemented

--- routes/planner.js
+++ routes/planner.js
@@ -7,9 +7,20 @@ router.get('/', function (req, res) {
   res.render('planner', context);
 });
 
+//Process Requests from the planner html form and redirect information to travelInterface
+router.post('/', function(req, res) {
+  let query = '?';
+  query += `dest=${req.body.destination}`;
+  query += `arrive=${req.body.arriveTime}`;
+  query += `return=${req.body.returnTime}`;
+  query += `ride=${req.body.rideSetting}`;
+  query += `notify=${req.body.notificationSetting}`
+  res.redirect(`/travelInterface${query}`)
+})
+
 module.exports = router
 
-//estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin 
+//estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin
 //and destination locations passed to it by using the AutoDriving system's mapping trip management software
 function estimateTripFuelUse(origin, destination){
   distance = bedroomMobile.autoDriver.getDistance(origin, destination);
