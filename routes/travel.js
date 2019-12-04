641781913170507b843e9e263d11c7b0dd4b785b
cweng777 <cweng777@gmail.com>
2019-12-04 13:15:37 +0800

    update files for Change destination during travel

--- routes/travel.js
+++ routes/travel.js
@@ -4,9 +4,24 @@ var router = express.Router();
 // define the home page route
 router.get('/', function (req, res) {
   let context = {};
+  //console.log(req.query.dest); //test 
   context.title="View the Current Bedroom-Mobile Route and Position";
   //context.greeting ="Enter your criteria";
-  context.route = {time:"8pm Friday Morning",stop:"Grandma's House"};
+  //context.route = {time:"8pm Friday Morning",stop:"Grandma's House"};
+  if(req.query.dest != undefined)
+  {
+  	  //highly coupled with planer, not good, need to think a better way later
+  	  var destination;
+  	  switch(req.query.dest) 
+  	  {
+  	  	case "1":
+  	  		destination = "Las Vegas"
+  	  		break;
+  	  }               //most attributes here are used to convey parameters back to planner page
+      context.route = {time:req.query.arrive, stop:destination, dest:req.query.dest, return:req.query.return, ride:req.query.ride, notify:req.query.notify};
+  }
+  //console.log(context); //test context under different conditions
+  
   //context.registeredDocks = [{name: "exampleName", cost: 9.99}, {name: "exampleName2", cost: 9.99}, {name: "exampleName3", cost: 9.99}];
 
   res.render('TravelPage', context);
