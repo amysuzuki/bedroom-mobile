e7e4286a1c1604b9a133a79c9708aafd2a22b8ce
nstapler <staplern@bu.edu>
2019-12-04 23:23:14 -0800

    Start handlebars utility data intake/ chart prep

--- routes/utilities.js
+++ routes/utilities.js
@@ -4,6 +4,31 @@ var router = express.Router()
 // define the home page route
 router.get('/', function (req, res) {
   let context = {};
+  context.electricity={
+    current:70,
+    max:100,
+    saverStatus: 1
+  };
+  context.water={
+    current:70,
+    max:100,
+    saverStatus:1
+  };
+  context.wifi={
+    current:70,
+    max:100,
+    status:1
+  };
+  context.lights={
+    status:1
+  };
+  context.room={
+    bed:1,
+    table:1
+  };
+  context.door={
+    locked:1
+  };
   res.render('utilities', context);
 });
 
