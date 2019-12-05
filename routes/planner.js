e49c2c22e95c4456eb21b32df2c1d4c71412bc1d
Phil <knamustarsunder@gmail.com>
2019-12-04 18:19:54 -0800

    tweaked stops required function
    (to no avail)

--- routes/planner.js
+++ routes/planner.js
@@ -4,6 +4,7 @@ var router = express.Router()
 // define the home page route
 router.get('/', function (req, res) {
   let context = {};
+
   //console.log(req.query.dest); //test if there are parameters with the request
   if(req.query.dest != undefined)
   {
@@ -13,8 +14,13 @@ router.get('/', function (req, res) {
     {
       case "1":
         destOptions.first = "1"
+        destOptions.firstDistance = 100;
         break;
+      default: 
+        //do nothing
     } 
+
+
 
     //rideSetting options
     var rideOptions = {};
@@ -42,7 +48,11 @@ router.get('/', function (req, res) {
         notifyOptions.third = "3"
         break;
     } 
-    context = {dest:destOptions, arrive:req.query.arrive, return:req.query.return, ride:rideOptions, notify:notifyOptions};
+    context = {dest:destOptions, arrive:req.query.arrive, return:req.query.return, ride:rideOptions, notify:notifyOptions, bedroomMobile:null};
+    let autoDriver= {stopsRequired:0, avgMPG:30, fuelRequired:50};
+    autoDriver.stopsRequired = estimateTripFuelUse(0, context.destOptions.firstDistance, autoDriver);
+    context.bedroomMobile.autoDriver=autoDriver;
+    console.log(context); //test context under different conditions
   }
   //console.log(context); //test context under different conditions
   res.render('planner', context);
@@ -63,9 +73,11 @@ module.exports = router
 
 //estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin
 //and destination locations passed to it by using the AutoDriving system's mapping trip management software
-function estimateTripFuelUse(origin, destination){
-  distance = bedroomMobile.autoDriver.getDistance(origin, destination);
-  fuelRequired = distance * bedroomMobile.utilities.avgMPG;
-  numRefuels = fuelRequired/ bedroomMobile.utilities.fuelCapacity;
+function estimateTripFuelUse(origin, destination, autoDriver){
+  //distance = bedroomMobile.autoDriver.distance;
+  console.log("calcing stops");
+  distance = destination - origin;
+  fuelRequired = distance * autoDriver.avgMPG;
+  numRefuels = fuelRequired/ autoDriver.fuelCapacity;
   return numRefuels;
 }
