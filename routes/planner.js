bb3136faa3fb0fbedfe61ac0c3f6216b3ca35a3c
Amy Suzuki <amy.s.suzuki@gmail.com>
2019-12-05 20:07:08 -0800

    Implement estimateTripFuel use for /travelInterface. Remove from /planner

--- routes/planner.js
+++ routes/planner.js
@@ -5,38 +5,28 @@ var router = express.Router()
 router.get('/', function (req, res) {
   let context = {};
 
-  //console.log(req.query.dest); //test if there are parameters with the request
   if(req.query.dest != undefined)
   {
     //destination options
     var destOptions = {};
-    switch(req.query.dest) 
+
+    switch(req.query.dest)
     {
       case "1":
         destOptions.first = "1"
-        destOptions.firstDistance = 100;
         break;
-      default: 
+      default:
         //do nothing
-    } 
+    }
 
-
-
-    //rideSetting options
-    var rideOptions = {};
-    switch(req.query.ride) 
-    {
-      case "1":
-        rideOptions.first = "1"
-        break;
-      case "2":
-        rideOptions.second = "2"
-        break;
-    } 
+    context.rideOptions = req.query.ride
 
     //notify options
+    /*
+    Same Comment as rideOptions
+    */
     var notifyOptions = {};
-    switch(req.query.notify) 
+    switch(req.query.notify)
     {
       case "1":
         notifyOptions.first = "1"
@@ -47,13 +37,17 @@ router.get('/', function (req, res) {
       case "3":
         notifyOptions.third = "3"
         break;
-    } 
-    context = {dest:destOptions, arrive:req.query.arrive, return:req.query.return, ride:rideOptions, notify:notifyOptions, bedroomMobile:null};
-    let autoDriver= {stopsRequired:0, avgMPG:30, fuelRequired:50};
-    autoDriver.stopsRequired = estimateTripFuelUse(0, context.destOptions.firstDistance, autoDriver);
-    context.bedroomMobile.autoDriver=autoDriver;
-    console.log(context); //test context under different conditions
+    }
+
+    context = {
+      dest:destOptions,
+      arrive:req.query.arrive,
+      return:req.query.return,
+      ride:rideOptions,
+      notify:notifyOptions
+    };
   }
+
   //console.log(context); //test context under different conditions
   res.render('planner', context);
 });
@@ -66,18 +60,8 @@ router.post('/', function(req, res) {
   query += `&return=${req.body.returnTime}`;
   query += `&ride=${req.body.rideSetting}`;
   query += `&notify=${req.body.notificationSetting}`
+
   res.redirect(`/travelInterface${query}`)
 })
 
 module.exports = router
-
-//estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin
-//and destination locations passed to it by using the AutoDriving system's mapping trip management software
-function estimateTripFuelUse(origin, destination, autoDriver){
-  //distance = bedroomMobile.autoDriver.distance;
-  console.log("calcing stops");
-  distance = destination - origin;
-  fuelRequired = distance * autoDriver.avgMPG;
-  numRefuels = fuelRequired/ autoDriver.fuelCapacity;
-  return numRefuels;
-}
--- routes/travel.js
+++ routes/travel.js
@@ -7,20 +7,39 @@ router.get('/', function (req, res) {
   context.title="View the Current Bedroom-Mobile Route and Position";
   //context.greeting ="Enter your criteria";
   //context.route = {time:"8pm Friday Morning",stop:"Grandma's House"};
-  if(req.query.dest != undefined && req.query.dest != "")
-  {
+
+  if(req.query.dest != undefined && req.query.dest != "") {
   	  //highly coupled with planer, not good, need to think a better way later
   	  var destination;
-  	  switch(req.query.dest) 
+  	  switch(req.query.dest)
   	  {
   	  	case "1":
   	  		destination = "212 Gass Avenue, Las Vegas, NV 89101 The Arts District, Las Vegas, Downtown Las Vegas Las Vegas Nevada United States"
   	  		break;
-  	  }               //most attributes here are used to convey parameters back to planner page
-      context.route = {arrivalTime:req.query.arrive, destination:destination, origin:"1401 F Street, San Diego, CA 92101 San Diego, East Village, Downtown San Diego San Diego California United States", departureTime:""
-                         , dest:req.query.dest, return:req.query.return, ride:req.query.ride, notify:req.query.notify};
-      context.pitstop = [{address:"West 21st Street, San Bernardino, CA 92411 San Bernardino San Bernardino California United States",latitude:"34.13471873440743",longitude:"-117.3139584183148",duration:15}];                   
+  	  }
+      //most attributes here are used to convey parameters back to planner page
+
+      context.route = {
+        arrivalTime:req.query.arrive,
+        destination:destination,
+        origin:"1401 F Street, San Diego, CA 92101 San Diego, East Village, Downtown San Diego San Diego California United States",
+        departureTime:"",
+        dest:req.query.dest,
+        return:req.query.return,
+        ride:req.query.ride,
+        notify:req.query.notify};
+
+      context.pitstop = [{address:"West 21st Street, San Bernardino, CA 92411 San Bernardino San Bernardino California United States",latitude:"34.13471873440743",longitude:"-117.3139584183148",duration:15}];
+
+      let autoDriver = {
+        avgMPG: 30,
+        fuelCapacity: 50
+      }
+      autoDriver.stopsRequired = estimateTripFuelUse(0, 100, autoDriver);
+      context.bedroomMobile = {};
+      context.bedroomMobile.autoDriver = autoDriver;
   }
+
   //context.route = {arrivalTime:"8pm Friday Morning",destination:"212 Gass Avenue, Las Vegas, NV 89101 The Arts District, Las Vegas, Downtown Las Vegas Las Vegas Nevada United States",origin:"1401 F Street, San Diego, CA 92101 San Diego, East Village, Downtown San Diego San Diego California United States",departureTime:""};
   //context.pitstop = [{address:"West 21st Street, San Bernardino, CA 92411 San Bernardino San Bernardino California United States",latitude:"34.13471873440743",longitude:"-117.3139584183148",duration:15}];
   //context.registeredDocks = [{name: "exampleName", cost: 9.99}, {name: "exampleName2", cost: 9.99}, {name: "exampleName3", cost: 9.99}];
@@ -29,3 +48,15 @@ router.get('/', function (req, res) {
 });
 
 module.exports = router;
+
+//estimateTripFuelUse() calculates how many fuel stops are required for a trip based on the origin
+//and destination locations passed to it by using the AutoDriving system's mapping trip management software
+function estimateTripFuelUse(origin, destination, autoDriver){
+  //distance = bedroomMobile.autoDriver.distance;
+  console.log("Calculating stops");
+  distance = destination - origin;
+  fuelRequired = distance * autoDriver.avgMPG;
+
+  numRefuels = fuelRequired/ autoDriver.fuelCapacity;
+  return numRefuels;
+}
